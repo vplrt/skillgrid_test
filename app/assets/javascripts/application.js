a2c3151fa92ace577327ddcd6c3eb723ab1d68aa
vplrt <djangositedev@gmail.com>
2015-09-16 02:24:34 +0400

    [Add] and [Configure] masonry-rails [Add] application syles [Add] styles to index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require masonry/jquery.masonry
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
