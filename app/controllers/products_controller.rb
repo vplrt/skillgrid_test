023f63e5d2c376bdf5d362ae5df0976f32db98c9
vplrt <djangositedev@gmail.com>
2015-09-16 01:10:53 +0400

    [Add] image to Product with Paperclip

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
   private
 
     def product_params
-      params.require(:product).permit(:title, :description)
+      params.require(:product).permit(:title, :description, :image)
     end
 
     def find_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :user
 
+  has_attached_file :image, :styles => { :medium => "300x300>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   validates :title, presence: true
   validates :description, presence: true
 end
