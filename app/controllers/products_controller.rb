529ba6c481237064eb5b0c0827dc5d9b21631911
vplrt <djangositedev@gmail.com>
2015-09-15 23:28:51 +0400

    [Add] resource Product with create and read abilities. [Change] html template to haml

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,34 @@
+class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all.order("created_at DESC")
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product, notice: "The product has been successfully created."
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:title, :description)
+    end
+
+    def find_product
+      @product = Product.find(params[:id])
+    end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :product do
+    title "MyString"
+description "MyText"
+  end
+
+end
