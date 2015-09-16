e4ed3b17a9356d21a6cdeef148f2fd2eed7c581c
vplrt <djangositedev@gmail.com>
2015-09-16 12:29:11 +0400

    [Add] seeds.rb for development db populating.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
+
+user = User.new(
+    name: "Valera",
+    email: "1@1.ru",
+    password: "11111111",
+    password_confirmation: "11111111",
+)
+user.save
+
+
+9.times do
+  user = User.new(
+    name: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    password: "11111111",
+    password_confirmation: "11111111",
+  )
+  user.save
+end
+
+100.times do
+  product = Product.new(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence(70),
+    user_id: rand(1..10),
+  )
+  product.save
+end
