89d5f5d0d0d712e0f50a1833d44dc38dd196e138
vplrt <djangositedev@gmail.com>
2015-09-16 15:07:03 +0400

    [Add] helpers for base title and full title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def base_title
+    "SkillgridTest"
+  end
+
+  def full_title(base_title, page_title)
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
