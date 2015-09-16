e5f1b6b23ce6de3961c07751f53f94c36baa78a8
vplrt <djangositedev@gmail.com>
2015-09-16 13:39:35 +0400

    [Add] User and Product factories for specs.

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :product do
-    title "MyString"
-description "MyText"
+    association :user
+    sequence(:title)  { |n| "Some product #{n}." }
+    sequence(:description)  { |n| "Description of this great product. #{n}." }
+    image { fixture_file_upload(Rails.root.join('spec', 'support', 'test.png'), \
+      'image/jpg') }
   end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:name)  { |n| "Test User #{n}" }
+    sequence(:email)  { |n| "test#{n}@example.com" }
+    password "please123"
   end
-
 end
