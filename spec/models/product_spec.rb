5251225fdf3cd2188ab34683681f552b8aa5ac58
vplrt <djangositedev@gmail.com>
2015-09-16 13:49:25 +0400

    [Add] Product model specs.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Product do
+  it "has a valid factory" do
+    expect(build(:product)).to be_valid
+  end
+
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :description }
+
+  it { should have_attached_file(:image) }
+  it { should validate_attachment_content_type(:image).
+              allowing('image/png', 'image/gif', 'image/jpeg').
+              rejecting('text/plain', 'text/xml', 'some_image/png') }
+
+  it { should belong_to :user }
+
 end
