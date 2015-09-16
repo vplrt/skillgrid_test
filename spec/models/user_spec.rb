1d0f49a1ed0d722ac9153d3066cb71cdc3fa0448
vplrt <djangositedev@gmail.com>
2015-09-16 13:51:41 +0400

    [Add] User model specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
+
+  before(:each) { @user = User.new(name: "Eric", email: 'user@example.com', password: "secret123") }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { should respond_to(:name) }
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_uniqueness_of(:name) }
+
 end
