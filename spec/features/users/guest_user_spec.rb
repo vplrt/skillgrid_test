32b77a5107b0cda3bb4a350584e430675bfd75fd
vplrt <djangositedev@gmail.com>
2015-09-18 23:20:16 +0400

    [Add] spec: simple user cant see company name

--- spec/features/users/guest_user_spec.rb
+++ spec/features/users/guest_user_spec.rb
@@ -4,6 +4,8 @@ feature "Guest user:" do
 
   before :each do
     @product = create(:pro)
+    @product.company = @product.seller.company
+    @product.save
     @user = create(:user)
   end
 
@@ -17,6 +19,18 @@ feature "Guest user:" do
     signin(@user.email, @user.password, :user)
     visit products_path @product
     expect(page).to have_content "Pro"
+  end
+
+  scenario "cant see campany name on products#index page" do
+    signin(@user.email, @user.password, :user)
+    visit root_path
+    expect(page).not_to have_content @product.company
+  end
+
+  scenario "cant see campany name on products#show page" do
+    signin(@user.email, @user.password, :user)
+    visit products_path @product
+    expect(page).not_to have_content @product.company
   end
 end
 
