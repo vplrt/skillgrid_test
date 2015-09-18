44785cecbad378f74437cffc2e646b931c95f210
vplrt <djangositedev@gmail.com>
2015-09-18 14:46:38 +0400

    [Add] default image url to sellers avatars

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -9,7 +9,7 @@ class Seller < ActiveRecord::Base
     thumb: '24x24>',
     square: '200x200#',
     medium: '300x300>'
-  }
+  }, default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates :company, presence: true
