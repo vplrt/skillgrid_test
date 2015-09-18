c79252cd87ea45eb2054e27489f1415526c1797a
vplrt <djangositedev@gmail.com>
2015-09-18 13:46:14 +0400

    [Add] and [Configure] Admin model [Add] admin registrations templates

---
+++ app/models/admin.rb
@@ -0,0 +1,16 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 10..128
+
+  has_many :products, dependent: :destroy
+  has_attached_file :avatar, styles: {
+    thumb: '24x24>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # validates :name, presence: true
+end
